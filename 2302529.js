40481c525c6fd13d6da76cfa710f69bff20ec0ab
권지연 <133744581+j-udi@users.noreply.github.com>
2023-11-29 16:04:36 +0900

    Add files via upload

---
+++ 2302529.js
@@ -0,0 +1,19 @@
+//객체 자료형
+let number = new Number(273);
+let string = new String('안녕하세요');
+let boolean = new Boolean(true);
+
+// 자료형을 출력합니다.
+console.log(typeof number);
+console.log(typeof string);
+console.log(typeof boolean);
+
+let number2 = 273.5210332;
+
+console.log(number.toFixed(1));
+console.log(number.toFixed(4));
+
+let string2 = 'abcdefg';
+
+string = string.toUpperCase();
+console.log(string);
